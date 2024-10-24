cc4a3bc168012654b7932560af5a9951767a1c4d
LouJK <loujk52@gmail.com>
2024-10-24 08:52:46 +0700

    Update install_allora.sh

--- install_allora.sh
+++ install_allora.sh
@@ -45,7 +45,7 @@ cd allora-chain
 # Replace requirements.txt with docker-compose.yaml
 echo -e "${LIGHT_BLUE}Replace with the new docker-compose.yaml...${RESET}"
 rm -rf docker-compose.yaml
-wget -q https://raw.githubusercontent.com/0xtnpxsgt/Allora-Full-Node-Setup/main/docker-compose.yaml -O /root/allora-chain/docker-compose.yaml
+wget -q https://raw.githubusercontent.com/louJK/Allora-Full-Node-Setup/main/docker-compose.yaml -O /root/allora-chain/docker-compose.yaml
 
 # Pull and start Docker containers
 echo -e "${LIGHT_BLUE}Pulling Docker images and starting containers...${RESET}"
